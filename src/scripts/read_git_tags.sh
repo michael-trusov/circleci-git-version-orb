96893eb79ac4a427b6614b2c3f0f1098bc7fc943
Michael Trusov <Trusov.michael@gmail.com>
2025-02-14 20:21:23 +0200

    Fix default values

--- src/scripts/read_git_tags.sh
+++ src/scripts/read_git_tags.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 #shellcheck disable=all
 
+### Description
+###   This function print message if INPUT_PARAM_DEBUG is set to 'true'
+###
+### Parameters:
+###   $1 - message
+debug_message() {
+    local message=$1
+
+    if [[ "${INPUT_PARAM_DEBUG}" == "true" ]]; then
+        echo "[DEBUG] $message"
+    fi
+}
+
+### Description
+###   This function print mesarray state if INPUT_PARAM_DEBUG is set to 'true'
+###
+### Parameters:
+###   $1 - message
+###   $2 - reference to array
+debug_array_state() {
+    local message=$1
+    local -n array=$2
+
+    if [[ "${INPUT_PARAM_DEBUG}" == "true" ]]; then
+        echo "[DEBUG] $message: ${array[@]}"
+    fi
+}
+
 # get Git tags and store them in an array
 mapfile -t tags < <(git tag -l)
+debug_array_state "Collection of git tags read from the repo" tags
 
 # join array elements into a space-separated string
 tags_string="${tags[*]}"
