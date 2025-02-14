cfc2812ed8e66094d1b0772451e72f8277060937
Michael Trusov <trusov.michael@gmail.com>
2025-02-14 21:28:49 +0200

    Feature/use git tags (#2)
    * Add extra test scenario
    * Refactor job to provide an ability automate receiving git tags
    * Rename job to 'use-git-tags-for-calculation'

---
+++ src/scripts/read_git_tags.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#shellcheck disable=all
+
+### Description
+###   This function print message if INPUT_PARAM_DEBUG is set to 'true'
+###
+### Parameters:
+###   $1 - message
+debug_message() {
+    local message=$1
+
+    if [[ "${INPUT_PARAM_DEBUG}" == "true" ]]; then
+        echo "[DEBUG] $message"
+    fi
+}
+
+### Description
+###   This function print mesarray state if INPUT_PARAM_DEBUG is set to 'true'
+###
+### Parameters:
+###   $1 - message
+###   $2 - reference to array
+debug_array_state() {
+    local message=$1
+    local -n array=$2
+
+    if [[ "${INPUT_PARAM_DEBUG}" == "true" ]]; then
+        echo "[DEBUG] $message: ${array[@]}"
+    fi
+}
+
+# Note: get Git tags and store them in an array
+mapfile -t tags < <(git tag -l)
+debug_array_state "Collection of git tags read from the repo" tags
+
+# Note: join array elements into a space-separated string
+tags_string="${tags[*]}"
+
+# Note: save to file
+echo "$tags_string" > "${INPUT_PARAM_INPUT_FOLDER}/${INPUT_PARAM_INPUT_FILE}"
