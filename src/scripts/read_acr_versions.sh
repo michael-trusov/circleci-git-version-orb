58b54cb137f9488cc5d52d972335d192eb56f1a3
Michael Trusov <Trusov.michael@gmail.com>
2025-02-25 15:31:31 +0200

    Add env. variables in context

--- src/scripts/read_acr_versions.sh
+++ src/scripts/read_acr_versions.sh
@@ -9,6 +9,10 @@ debug_message() {
     fi
 }
 
+debug_message "ACR_USERNAME=$ACR_USERNAME"
+debug_message "REPOSITORY=$REPOSITORY"
+debug_message "REGISTRY=$REGISTRY"
+
 current_version_meta_data=$(az acr manifest list-metadata --username $ACR_USERNAME --password $ACR_PASSWORD --registry $REGISTRY --name $REPOSITORY --top 1 --orderby time_desc)
 current_latest_tag=$(echo "$current_version_meta_data" | jq -r '.[0].tags[0]')
 debug_message "current_latest_tag: $current_latest_tag"
