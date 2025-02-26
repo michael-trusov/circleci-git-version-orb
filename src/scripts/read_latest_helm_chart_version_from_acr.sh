e0047076b853fffe97bd5b18366a989d24f37dba
Michael Trusov <Trusov.michael@gmail.com>
2025-02-26 12:43:10 +0200

    Rename  scripts/read_acr_versions.sh

--- src/scripts/read_latest_helm_chart_version_from_acr.sh
+++ src/scripts/read_latest_helm_chart_version_from_acr.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #shellcheck disable=all
 
+### Description
+###   This function print message if INPUT_PARAM_DEBUG is set to 'true'
+###
+### Parameters:
+###   $1 - message
 debug_message() {
     local message=$1
 
@@ -9,14 +14,18 @@ debug_message() {
     fi
 }
 
-debug_message "ACR_USERNAME=$ACR_USERNAME"
-debug_message "REPOSITORY=$REPOSITORY"
-debug_message "REGISTRY=$REGISTRY"
+debug_message "[ENV VAR] ACR_USERNAME=$ACR_USERNAME"
+debug_message "[ENV VAR] REPOSITORY=$REPOSITORY"
+debug_message "[ENV VAR] REGISTRY=$REGISTRY"
 
+# Note: get metadata of the latest Helm chart stored in repository
 current_version_meta_data=$(az acr manifest list-metadata --username $ACR_USERNAME --password $ACR_PASSWORD --registry $REGISTRY --name $REPOSITORY --top 1 --orderby time_desc)
+# Note: extract the tag version, i.e. 1.0.0-beta.2
 current_latest_tag=$(echo "$current_version_meta_data" | jq -r '.[0].tags[0]')
+
+# Note: add 'v' prefix to the tag version, i.e. v1.0.0-beta.2
 current_latest_tag="v${current_latest_tag}"
 debug_message "current_latest_tag: $current_latest_tag"
 
-# Note: save to file
+# Note: save tag in file
 echo "$current_latest_tag" > "${INPUT_PARAM_INPUT_FOLDER}/${INPUT_PARAM_INPUT_FILE}"
